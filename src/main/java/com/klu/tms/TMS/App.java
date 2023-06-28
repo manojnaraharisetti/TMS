4834dfe8bc8ca519ae4f02ac69277aaf82097e2a
manojnaraharisetti <2100032451@kluniversity.in>
2023-06-28 11:40:12 +0530

    creating a project..

---
+++ src/main/java/com/klu/tms/TMS/App.java
@@ -0,0 +1,13 @@
+package com.klu.tms.TMS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!");
+    }
+}
---
+++ src/main/java/com/klu/tms/TMS/sub.java
@@ -0,0 +1,7 @@
+package com.klu.tms.TMS;
+
+public class sub {
+	public void manoj() {
+		
+	}
+}
